69d2a4de282d898b6699c800e84000532936228d
mutaihillary <mutaihillary@yahoo.com>
2016-07-18 08:38:17 +0300

    update

--- js/app.js
+++ js/app.js
@@ -1,1 +1,11 @@
 $(document).foundation();
+
+/**********chat js***************/
+
+// based on http://www.materialup.com/posts/dailyui-13-direct-messaging
+// will add emojis soon http://codepen.io/genarocolusso/pen/QEajPy
+
+$(".head").on("click", function(){
+   $(".friends_list").toggleClass("active");
+
+})
